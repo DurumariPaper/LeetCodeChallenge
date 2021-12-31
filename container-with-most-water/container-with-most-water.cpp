1813a09ca13f32b96881187077ba0a121a983623
DurumariPaper <96913120+DurumariPaper@users.noreply.github.com>
2021-12-31 23:31:20 +0900

    Time: 84 ms (63.35%), Space: 59.1 MB (36.71%) - LeetHub

---
+++ container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maxArea(vector<int> &height) {
+        
+        int maxarea = 0, l = 0, r = height.size() - 1;
+        while (l < r) {
+            maxarea = std::max(maxarea, std::min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r])
+                l++;
+            else
+                r--;
+        }
+        return maxarea;
+    }
+};
