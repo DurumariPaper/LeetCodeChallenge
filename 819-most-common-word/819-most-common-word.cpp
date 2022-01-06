a9928275402edee6b0a855466ab48cd0dfb4f721
DurumariPaper <96913120+DurumariPaper@users.noreply.github.com>
2022-01-07 00:01:07 +0900

    Time: 4 ms (86.67%), Space: 7.6 MB (80.91%) - LeetHub

---
+++ 819-most-common-word/819-most-common-word.cpp
@@ -0,0 +1,51 @@
+class Solution {
+public:
+    string mostCommonWord(string paragraph, vector<string>& banned) {
+        unordered_map<string, int> wordMap;
+        
+
+        
+        string word = "";
+        for(char c : paragraph)
+        {
+            if(c >= 'a' && c <= 'z')
+            {
+                word += c;
+            }
+            else if( c >= 'A' && c <= 'Z')
+            {
+                word += tolower(c);
+            }
+            else
+            {
+                if(word != "")
+                {                    
+                    wordMap[word]++;
+                    word = "";
+                }
+            }
+        }
+        
+        if(word != "")
+        {
+            wordMap[word]++;
+        }
+        
+        for( string str : banned)
+        {
+            wordMap.erase(str);
+        }
+        
+        string result = "";
+        int max = 0;
+        for(auto it = wordMap.begin(); it != wordMap.end() ; it++)
+        {
+            if(it->second > max)
+            {
+                max = it->second;
+                result = it->first;
+            }
+        }
+        return result;
+    }
+};
