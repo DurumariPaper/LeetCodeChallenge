aaee067b7a59697e4ad7d18bd18d947a38392cc9
DurumariPaper <96913120+DurumariPaper@users.noreply.github.com>
2022-01-19 00:46:35 +0900

    Time: 60 ms (27.51%), Space: 12.7 MB (38.82%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,59 @@
+class Solution {
+    int count = 0;
+    vector<vector<bool>> visited;
+    int m;
+    int n;
+    
+    int x[4] = {-1,0,1,0};
+    int y[4] = {0,1,0,-1};
+public:
+    void findIsland(vector<vector<char>>& grid, int i, int j)
+    {
+        visited[i][j] = true;
+        
+        for(int k =0; k < 4; k++)
+        {
+            int nextX = i + x[k];
+            int nextY = j + y[k];
+            
+            if(nextX >= 0 && nextX < m && nextY >= 0 && nextY < n)
+            {
+                if(!visited[nextX][nextY] && grid[nextX][nextY] == '1')
+                {
+                    findIsland(grid,nextX,nextY);
+                }
+            }
+        }
+    }
+    
+    
+    int numIslands(vector<vector<char>>& grid) {
+        
+        //초기화
+        m = grid.size();
+        n = grid[0].size();
+        for(int i = 0; i < m; i++)
+        {
+            vector<bool> visit;
+            for(int j = 0; j < n; j++)
+            {
+                visit.push_back(false);
+            }
+            visited.push_back(visit);
+        }
+                
+        for(int i = 0; i < m; i++)
+        {
+            for(int j = 0; j < n; j++)
+            {
+                if(visited[i][j] == false && grid[i][j] == '1')
+                {
+                    findIsland(grid, i, j);
+                    count++;
+                }
+            }
+        }
+        
+        return count;
+    }
+};
