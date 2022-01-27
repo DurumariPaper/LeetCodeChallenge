3e2f0cd3c11b8a8c46fa990a86ebe948996a9d0b
DurumariPaper <96913120+DurumariPaper@users.noreply.github.com>
2022-01-28 00:03:40 +0900

    Time: 18 ms (37.46%), Space: 8.3 MB (83.82%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -1,24 +1,17 @@
 class Solution {
 public:
     string frequencySort(string s) {
-        unordered_map<char,int> freq;
-        vector<string> bucket(s.size()+1, "");
-        string res;
-        
-        //count frequency of each character
-        for(char c:s) freq[c]++;
-        //put character into frequency bucket
-        for(auto& it:freq) {
-            int n = it.second;
-            char c = it.first;
-            bucket[n].append(n, c);
+        unordered_map<char,int> freq;           //for frequency of characters
+        priority_queue<pair<int,char>> maxheap; //maxheap according to frequency of characters
+        for(char c: s)
+            freq[c]++;
+        for(auto it: freq)
+            maxheap.push({it.second,it.first}); //heap will be constructed on the basis of frequency
+        s="";   
+        while(!maxheap.empty()){
+            s+=string(maxheap.top().first,maxheap.top().second); //frequency times the character
+            maxheap.pop();
         }
-        //form descending sorted string
-        for(int i=s.size(); i>0; i--) {
-            if(!bucket[i].empty())
-                res.append(bucket[i]);
-        }
-        return res;
-        
+        return s;
     }
 };
