69845b1e9dbd802f8ef6b2c4b03805f2c875f5ba
DurumariPaper <96913120+DurumariPaper@users.noreply.github.com>
2022-05-29 23:14:41 +0900

    Time: 118 ms (84.97%), Space: 67.8 MB (55.05%) - LeetHub

--- 53-maximum-subarray/53-maximum-subarray.cpp
+++ 53-maximum-subarray/53-maximum-subarray.cpp
@@ -1,44 +1,17 @@
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-        int currentValue = nums[0];
-        int maxValue = nums[0];
+        
+        int currentArray = nums[0];
+        int maxArray = nums[0];
         
         for(int i = 1 ; i < nums.size(); i++)
         {
             int num = nums[i];
-            currentValue = max(num, currentValue + num);
-            maxValue = max(maxValue, currentValue);
-            
+            currentArray = max(num, currentArray + num);
+            maxArray = max(maxArray, currentArray);            
         }
-        return maxValue;
+        
+        return maxArray;
     }
 };
-
-
-/*
-    int maxSubArray(vector<int>& nums) {
-        const int size = nums.size();
-        vector<vector<int>> dp( size , vector<int> (size, 0));
-        if(size == 1)
-            return nums[0];
-        if(size == 0)
-            return 0;
-        int result = INT_MIN;
-        for(int i = 0; i < size; i++)
-        {
-            dp[i][i] = nums[i];
-            result = max(result, dp[i][i]);
-            
-        }
-        for(int gap = 1; gap < size; gap++)
-        {
-            for(int i = 0; i < size - gap; i++)
-            {
-                dp[i][i+gap] = dp[i][i+gap-1] + nums[i+gap];
-                result = max(result, dp[i][i+gap]);
-            }
-        }
-        return result;
-    }
-*/
